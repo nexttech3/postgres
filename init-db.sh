e6237b3a17996cf856105fa52c05db13976283c6
nexttech3 <143657091+nexttech3@users.noreply.github.com>
2024-11-28 19:54:22 -0300

    Delete init-db.sh

--- init-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Ative as extensões no banco de dados padrão 'postgres'
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE EXTENSION IF NOT EXISTS timescaledb;
-    CREATE EXTENSION IF NOT EXISTS vector;
-    CREATE EXTENSION IF NOT EXISTS plpython3u;
-EOSQL
